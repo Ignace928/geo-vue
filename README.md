10b907fd0e15f46a59c0c0221b7375ec275008a8
Edwardo928 <edwardoignace928@gmail.com>
2025-05-30 11:14:27 +0300

    Create simple App-vue-js

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vue 3 + TypeScript + Vite
+Mon projet TypeScript pour le CI/CD
+
+(npm install -D vitest @vitejs/plugin-vue @testing-library/vue )
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import LoginForm from './LoginForm.vue';
+import CarteVue from './Vbox/Carte.vue';
+
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <CarteVue/>
+    <div class="card">
+      <button type="button" @click="count++">count is {{ count }}</button>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
